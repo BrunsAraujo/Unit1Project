74f980045534dc45ad8a2cbf065d5a3950573abc
Bruno Araujogit <bruno.correadearaujo@gmail.com>
2025-10-27 10:35:03 -0500

    App2.jsx for Homepage rendering

---
+++ header/src/App2.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import CollapsibleMenu2 from "./components/CollapsibleMenu2";
+import LoginForm from "./components/LoginForm";
+
+function App() {
+  const handleLogin = (username) => {
+    alert(`Welcome, ${username}!`);
+  };
+
+  return (
+    <Router>
+      <Header />
+      <CollapsibleMenu2 />
+      <Routes>
+        <Route path="/login" element={<LoginForm onLogin={handleLogin} />} />
+        {/* Add other routes here if needed */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
