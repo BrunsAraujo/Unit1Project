fb6b491f36d57de30695d5a54e385b6c4623a42f
Bruno Araujogit <bruno.correadearaujo@gmail.com>
2025-11-12 15:01:39 -0600

    //adds link to the menu from react-router-dom for navigation, improves the nomeclature of the lables adds light blue padding border.

--- header/src/components/CollapsibleMenu2.jsx
+++ header/src/components/CollapsibleMenu2.jsx
@@ -1,3 +1,5 @@
+//adds link to the menu from react-router-dom for navigation, improves the nomeclature of the lables
+// adds light blue padding border.
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
@@ -5,23 +7,34 @@ function CollapsibleMenu2() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="collapsible-menu">
+    <div
+      className="collapsible-menu"
+      style={{
+        backgroundColor: "#f0f8ff", // Light blue background
+        padding: "20px",
+        borderBottom: "2px solid #ccc",
+        textAlign: "left"
+      }}
+    >
       <button className="toggle-button" onClick={() => setIsOpen(!isOpen)}>
         {isOpen ? "Close Menu" : "Open Menu"}
       </button>
 
       {isOpen && (
         <nav className="menu">
-          <ul>
+          <ul style={{ listStyle: "none", padding: 0 }}>
             <li><Link to="/">HOME PAGE</Link></li>
             <li><Link to="/about">ABOUT</Link></li>
-            <li><Link to="/search">SEARCH</Link></li>
-            <li><Link to="/cities">CITIES</Link></li>
-            <li><Link to="/login">USER LOGIN</Link></li>
+            <li><Link to="/search">SEARCH FOR A CITY</Link></li>
+            <li><Link to="/CityRandomizer">RANDOM CITY SELECTOR</Link></li>
+            <li><Link to="/cities">CITIES AVAILABLE</Link></li>
+            <li><Link to="/login">USER AREA</Link></li>
           </ul>
         </nav>
       )}
+    
     </div>
+    
   );
 }
 
